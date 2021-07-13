12f070f345b1baaa0c0217d7cd8040bef70ac497
joeldevel <christianjoel.developer@gmail.com>
2021-07-13 16:09:31 -0300

    basic functionality done

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+// ****************************************************
+//                      grab elements
+// ****************************************************
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('.clear-button');
 const toggleGapButton = document.querySelector('.toggle-gap');
-const containerWidth = '400';
-// let gridElements = null;
 
-// firstTime
-// newPlayground();
+const containerWidth = '400';
+//for first time round
 let squaresPerSide = 16;
+
+
+// wait grid items exists
+setTimeout(function(){
+  let gridElements = document.querySelectorAll('.container>div') ;
+  putElementsToListen(gridElements);
+},0);
+
+
 setGrid(squaresPerSide);
 populateGrid(squaresPerSide*squaresPerSide, containerWidth/squaresPerSide);
+
+
+// ****************************************************
+//                      functions
+// ****************************************************
 
 function setGrid(squares) {
   container.style.gridTemplateColumns=`repeat(${squares}, 1fr)`;
@@ -22,9 +37,25 @@ function populateGrid(amount, squareHeight) {
     container.appendChild(div);
   }
 }
-let gridElements = document.querySelectorAll('.container>div') ;
 
-// console.log(gridElements);
+function newPlayground() {
+  let newSquareSide = prompt('How many square per side?');
+  if( newSquareSide < 1 || newSquareSide > 100) {
+    newSquareSide = 16;
+  }
+  clearGrid();
+  setGrid(newSquareSide);
+  populateGrid(newSquareSide*newSquareSide, containerWidth/newSquareSide);
+  putElementsToListen(document.querySelectorAll('.container>div'));
+}
+
+function clearGrid() {
+  const elements = document.querySelectorAll('.container>div');
+  // console.log(elements);
+  for(let i =0; i< elements.length; i++) {
+    container.removeChild(elements[i]);
+  }
+}
 
 function paintElement() {
   this.classList.add('active');
@@ -34,11 +65,16 @@ function toggleGap() {
   container.classList.toggle('no-gap');
 }
 
+function putElementsToListen(elements) {
+  elements.forEach(item=>{
+    item.addEventListener('mouseover', paintElement);
+  });
+}
 
-// clearButton.addEventListener('click', newPlayground);
+// *************************************************
+//                   actions
+// ************************************************
+
+clearButton.addEventListener('click', newPlayground);
 
 toggleGapButton.addEventListener('click', toggleGap);
-
-gridElements.forEach(item=>{
-  item.addEventListener('mouseover', paintElement);
-});
