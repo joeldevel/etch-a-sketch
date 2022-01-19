98cdbfa4919fc320e9febd291d6c9ee0c66acbb7
joeldevel <christianjoel.developer@gmail.com>
2022-01-18 22:19:59 -0300

    adds styles and images

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ let squaresPerSide = 16;
 
 
 // wait grid items exists
-setTimeout(function(){
-  let gridElements = document.querySelectorAll('.container>div') ;
-  putElementsToListen(gridElements);
-},0);
+setTimeout(function() {
+    let gridElements = document.querySelectorAll('.container>div') ;
+    putElementsToListen(gridElements);
+}, 0);
 
 
 setGrid(squaresPerSide);
@@ -26,49 +26,48 @@ populateGrid(squaresPerSide*squaresPerSide, containerWidth/squaresPerSide);
 // ****************************************************
 
 function setGrid(squares) {
-  container.style.gridTemplateColumns=`repeat(${squares}, 1fr)`;
-  container.style.width = `${containerWidth}px`;
+    container.style.gridTemplateColumns=`repeat(${squares}, 1fr)`;
+    container.style.width = `${containerWidth}px`;
 }
 
 function populateGrid(amount, squareHeight) {
-  for(let i = 0; i< amount; i++) {
-    const div = document.createElement('div');
-    div.style.height =`${squareHeight}px`;
-    container.appendChild(div);
-  }
+    for(let i = 0; i < amount; i++) {
+        const div = document.createElement('div');
+        div.style.height =`${squareHeight}px`;
+        container.appendChild(div);
+    }
 }
 
 function newPlayground() {
-  let newSquareSide = prompt('How many square per side?');
-  if( newSquareSide < 1 || newSquareSide > 100) {
-    newSquareSide = 16;
-  }
-  clearGrid();
-  setGrid(newSquareSide);
-  populateGrid(newSquareSide*newSquareSide, containerWidth/newSquareSide);
-  putElementsToListen(document.querySelectorAll('.container>div'));
+    let newSquareSide = prompt('How many square per side?');
+    if( newSquareSide < 1 || newSquareSide > 100) {
+        newSquareSide = 16;
+    }
+    clearGrid();
+    setGrid(newSquareSide);
+    populateGrid(newSquareSide*newSquareSide, containerWidth/newSquareSide);
+    putElementsToListen(document.querySelectorAll('.container>div'));
 }
 
 function clearGrid() {
-  const elements = document.querySelectorAll('.container>div');
-  // console.log(elements);
-  for(let i =0; i< elements.length; i++) {
-    container.removeChild(elements[i]);
-  }
+    const elements = document.querySelectorAll('.container>div');
+    for(let i = 0; i < elements.length; i++) {
+        container.removeChild(elements[i]);
+    }
 }
 
 function paintElement() {
-  this.classList.add('active');
+    this.classList.add('active');
 }
 
 function toggleGap() {
-  container.classList.toggle('no-gap');
+    container.classList.toggle('no-gap');
 }
 
 function putElementsToListen(elements) {
-  elements.forEach(item=>{
-    item.addEventListener('mouseover', paintElement);
-  });
+    elements.forEach(item => {
+        item.addEventListener('mouseover', paintElement);
+    });
 }
 
 // *************************************************
@@ -78,3 +77,11 @@ function putElementsToListen(elements) {
 clearButton.addEventListener('click', newPlayground);
 
 toggleGapButton.addEventListener('click', toggleGap);
+
+
+
+// *************************************************
+//                   modify styles
+// ************************************************
+
+document.getElementById("panel").style.width = container.style.width;
